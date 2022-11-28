4fff6318028c34b16bbe687afcb811b07aef23a9
Dreamy Cecil <stalker27production@gmail.com>
2022-11-29 01:22:37 +0300

    Add Data library.

---
+++ Data/ByteSwap.hpp
@@ -0,0 +1,90 @@
+//! This file is a part of Dreamy Utilities.
+//! Licensed under the MIT license (see LICENSE file).
+
+#ifndef _DREAMYUTILITIES_INCL_BYTESWAP_H
+#define _DREAMYUTILITIES_INCL_BYTESWAP_H
+
+#include "../Base/Base.hpp"
+
+// Byte-swapping functions for modern MSVC compilers
+#if !_DREAMY_UNIX && _DREAMY_CPP11
+  #include <intrin.h>
+#endif
+
+namespace dreamy
+{
+  // Invert bytes of any class' content
+  template<typename Type> inline
+  Type ByteSwapT(Type valSrc)
+  {
+    typedef struct {
+      union {
+        Type _val;
+        c8 _bytes[sizeof(Type)];
+      };
+    } SwapData;
+
+    SwapData src;
+    SwapData dst;
+    src._val = valSrc;
+
+    for (u32 i = 0; i < sizeof(Type); i++)
+    {
+      dst._bytes[i] = src._bytes[sizeof(Type) - i - 1];
+    }
+
+    return dst._val;
+  };
+
+  // Byte-swapping functions for old MSVC compilers
+  #if !_DREAMY_UNIX && !_DREAMY_CPP11
+    // Invert bytes of a 16-bit integer
+    static inline u16 _byteswap_ushort(u16 i16) {
+      return ((i16 & 0xFF00) >> 8) | ((i16 & 0x00FF) << 8);
+    };
+
+    // Invert bytes of a 32-bit integer
+    static inline u32 _byteswap_ulong(u32 i32) {
+      return ((i32 & 0xFF000000) >> 24)
+           | ((i32 & 0x00FF0000) >>  8)
+           | ((i32 & 0x0000FF00) <<  8)
+           | ((i32 & 0x000000FF) << 24);
+    };
+
+    // Invert bytes of a 64-bit integer
+    #define _byteswap_uint64(i64) ByteSwapT<u64>(i64)
+  #endif
+
+  // Reverse the order of bytes in a 16-bit integer
+  __forceinline u16 ByteSwap16(u16 i16)
+  {
+    #if !_DREAMY_UNIX
+      return _byteswap_ushort(i16);
+    #else
+      return __builtin_bswap16(i16);
+    #endif
+  };
+
+  // Reverse the order of bytes in a 32-bit integer
+  __forceinline u32 ByteSwap32(u32 i32)
+  {
+    #if !_DREAMY_UNIX
+      return _byteswap_ulong(i32);
+    #else
+      return __builtin_bswap32(i32);
+    #endif
+  };
+
+  // Reverse the order of bytes in a 64-bit integer
+  __forceinline u64 ByteSwap64(u64 i64)
+  {
+    #if !_DREAMY_UNIX
+      return _byteswap_uint64(i64);
+    #else
+      return __builtin_bswap64(i64);
+    #endif
+  };
+
+};
+
+#endif // (Dreamy Utilities Include Guard)
---
+++ Data/DataDump.hpp
@@ -0,0 +1,117 @@
+//! This file is a part of Dreamy Utilities.
+//! Licensed under the MIT license (see LICENSE file).
+
+#ifndef _DREAMYUTILITIES_INCL_DATA_DUMP_H
+#define _DREAMYUTILITIES_INCL_DATA_DUMP_H
+
+#include "../Base/Base.hpp"
+
+#include "../Strings/Strings.hpp"
+#include <iomanip>
+
+namespace dreamy
+{
+  // Display raw data divided into multiple byte chunks
+  inline void DumpDataChunks(CStringStream &out, void *pData, size_t iStartOffset, size_t iChunkSize, size_t iChunks) {
+    // Pure byte array starting at some offset
+    u8 *pDataBytes = ((u8 *)pData) + iStartOffset;
+
+    // Chunk bytes
+    u8 *aChunk = new u8[iChunkSize];
+
+    // Go until the last byte
+    const size_t iBytes = iStartOffset + iChunks * iChunkSize;
+
+    for (size_t iOffset = iStartOffset; iOffset < iBytes; iOffset += iChunkSize) {
+      // Display current offset as an address
+      out.PrintF("%0*X", sizeof(size_t) * 2, iOffset);
+
+      // Reset formatting and separate output space
+      out << '\t';
+
+      // Output one chunk of data at the current offset byte by byte
+      for (size_t iChunk = 0; iChunk < iChunkSize; ++iChunk) {
+        aChunk[iChunk] = *(pDataBytes + iOffset + iChunk);
+
+        // Replace invalid characters with spaces
+        c8 chPrint = (aChunk[iChunk] < ' ') ? ' ' : aChunk[iChunk];
+        out << chPrint;
+      }
+
+      out << '\t';
+
+      // Output one chunk of data in pure bytes
+      for (size_t iPure = 0; iPure < iChunkSize; ++iPure) {
+        u32 iByte = (u32)aChunk[iPure];
+        out.PrintF("%02X", iByte);
+      }
+
+      // Next chunk
+      out << '\n';
+    }
+
+    delete[] aChunk;
+  };
+
+  // Print unsigned integers
+
+  inline Str_t DumpField(const u8 *pField, const c8 *strPrintAs = "%u") {
+    c8 str[128];
+    sprintf(str, strPrintAs, *pField);
+    return str;
+  };
+
+  inline Str_t DumpField(const u16 *pField, const c8 *strPrintAs = "%u") {
+    c8 str[128];
+    sprintf(str, strPrintAs, *pField);
+    return str;
+  };
+
+  inline Str_t DumpField(const u32 *pField, const c8 *strPrintAs = "%u") {
+    c8 str[128];
+    sprintf(str, strPrintAs, *pField);
+    return str;
+  };
+
+  inline Str_t DumpField(const u64 *pField, const c8 *strPrintAs = "%llu") {
+    c8 str[128];
+    sprintf(str, strPrintAs, *pField);
+    return str;
+  };
+
+  // Print signed integers
+
+  inline Str_t DumpField(const s8 *pField, const c8 *strPrintAs = "%d") {
+    c8 str[128];
+    sprintf(str, strPrintAs, *pField);
+    return str;
+  };
+
+  inline Str_t DumpField(const s16 *pField, const c8 *strPrintAs = "%d") {
+    c8 str[128];
+    sprintf(str, strPrintAs, *pField);
+    return str;
+  };
+
+  inline Str_t DumpField(const s32 *pField, const c8 *strPrintAs = "%d") {
+    c8 str[128];
+    sprintf(str, strPrintAs, *pField);
+    return str;
+  };
+
+  inline Str_t DumpField(const s64 *pField, const c8 *strPrintAs = "%lld") {
+    c8 str[128];
+    sprintf(str, strPrintAs, *pField);
+    return str;
+  };
+
+  // Print string
+  inline Str_t DumpField(const c8 **pField, const c8 *strPrintAs = "%s") {
+    c8 str[128];
+    sprintf(str, strPrintAs, *pField);
+    return str;
+  };
+
+};
+
+#endif // (Dreamy Utilities Include Guard)
