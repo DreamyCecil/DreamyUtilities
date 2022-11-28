3d524d1ef1792781fbd38d102bbce1df88f0091a
Dreamy Cecil <stalker27production@gmail.com>
2022-11-29 01:32:12 +0300

    Add JSON library.

---
+++ JSON/JSON.hpp
@@ -0,0 +1,32 @@
+//! This file is a part of Dreamy Utilities.
+//! Licensed under the MIT license (see LICENSE file).
+
+#ifndef _DREAMYUTILITIES_INCL_JSON_H
+#define _DREAMYUTILITIES_INCL_JSON_H
+
+#include "../Base/Base.hpp"
+
+// All JSON headers
+#include "../JSON/Builder.hpp"
+#include "../JSON/Tokenizer.hpp"
+
+namespace dreamy
+{
+  // Parse JSON string and output it in a variant with optional token list
+  inline void ParseJSON(CVariant &valJSON, CTokenList *paTokens, const Str_t &strJSON, const CValObject &oConstants = _jsonConstants.list) {
+    static CTokenList aTokenList;
+
+    // Supply local token list if none specified
+    if (paTokens == nullptr) {
+      aTokenList.clear();
+      paTokens = &aTokenList;
+    }
+
+    // Tokenize JSON string and build a value out of it
+    TokenizeJSON(*paTokens, strJSON, oConstants);
+    BuildJSON(valJSON, *paTokens);
+  };
+
+};
+
+#endif // (Dreamy Utilities Include Guard)
